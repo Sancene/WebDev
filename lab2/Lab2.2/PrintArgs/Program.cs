c6f34ae0793d7b430136bae600beecfe0f1a4d5f
Sancene <56795876+Sancene@users.noreply.github.com>
2020-03-19 17:29:09 +0300

    added lab2.2

---
+++ lab2/Lab2.2/PrintArgs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PrintArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No parameters were specified!");
+            }
+            else
+            {
+                Console.WriteLine("Number of arguments: " + args.Length);
+                Console.Write("Arguments: ");
+                foreach (string argument in args)
+                {
+                    Console.Write(argument + " ");
+                }
+            }
+        }
+    }
+}
