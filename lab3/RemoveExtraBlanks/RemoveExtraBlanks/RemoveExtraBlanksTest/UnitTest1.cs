e6d58f386851bf3d9aaa65f29033229d3f47663a
Sancene <56795876+Sancene@users.noreply.github.com>
2020-03-31 16:41:44 +0300

    changed tests

--- lab3/RemoveExtraBlanks/RemoveExtraBlanks/RemoveExtraBlanksTest/UnitTest1.cs
+++ lab3/RemoveExtraBlanks/RemoveExtraBlanks/RemoveExtraBlanksTest/UnitTest1.cs
@@ -4,46 +4,8 @@ using RemoveExtraBlanks;
 namespace RemoveExtraBlanksTest
 {
     [TestClass]
-    public class UnitTest1
+    public class RemoveExtraBlanksTests
     {
-        [TestMethod]
-        public void NoArguments()
-        {
-            string[] wrong1 = new string[0] {  };
-
-            int min = Program.Main(wrong1);
-
-            Assert.AreEqual(1, min);
-        }
-        [TestMethod]
-        public void TooManyArguments()
-        {
-            string[] wrong1 = new string[3] { "input.txt", "output.txt", "input2.txt"};
-
-            int min;
-            min = Program.Main(wrong1);
-
-            Assert.AreEqual(1, min);
-        }
-        [TestMethod]
-        public void FileWithExtraSpacesAndTabs()
-        {
-            string[] wrong1 = new string[2] { "Tests/input1.txt", "Tests/output.txt" };
-
-            Program.Main(wrong1);
-            string[] correctoutput = System.IO.File.ReadAllLines("Tests/correctoutput1.txt");
-            string[] output = System.IO.File.ReadAllLines("Tests/output.txt");
-            bool correct = true;
-
-            for (int i = 0; i < output.Length; i++)
-            {
-                if (output[i] != correctoutput[i])
-                    correct = false;
-            }
-
-            Assert.IsTrue(correct);
-        }
-
         [TestMethod]
         public void RemoveExtraBlanksEmptyString()
         {
@@ -79,6 +41,18 @@ namespace RemoveExtraBlanksTest
 
             //Assert
             Assert.AreEqual(testString, "");
+        }
+        [TestMethod]
+        public void RemoveExtraBlanksNoExtraBlanks()
+        {
+            //Arrange
+            string testString = "h e l\tl o";
+
+            //Act
+            testString = Program.RemoveExtraBlanks(testString);
+
+            //Assert
+            Assert.AreEqual(testString, "h e l\tl o");
         }
     }
 }
